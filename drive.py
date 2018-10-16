aebe2b0b43c793abafc0c3e03407d2d156a6e304
Jonas Karlsson <jonaskarlsson@Jonass-MacBook-Pro.local>
2018-10-16 17:00:11 +0200

    First version of listenForTarget

--- drive.py
+++ drive.py
@@ -1,14 +1,15 @@
 #! /usr/bin/env python
 import rospy
 from geometry_msgs.msg import Twist
+from geometry_msgs.msg import Pose
 import numpy as np
 
 class driver():
 
     def init(self):
         self.pub_twist = rospy.Publisher('/cmd_vel', Twist, queue_size=1)
-        self.lin = np.array([1,0,0])
-        self.ang = np.array([0,0,0.25])
+        self.targetX = 0;
+        self.targetY = 0;
         print("init")
 
     def run(self):
@@ -16,18 +17,27 @@ class driver():
             self.init()
             r = rospy.Rate(10)
             while not rospy.is_shutdown():
-		print("entering while..")
+
                 self.twist = Twist()
                 self.twist.linear.x = 1
                 self.twist.angular.z = 0.25
                 self.pub_twist.publish(self.twist)
-		print("published")
+		        print("published")
                 r.sleep()
         except rospy.exceptions.ROSInterruptException:
             pass
 
+    def listenForTarget(self):
+        self.target_subscriber = rospy.Subscriber('/target', Pose, updateTarget)
+
+
+    def updateTarget(msg):
+        self.targetX = msg.pose.position.x;
+        self.targetY = msg.pose.position.y;
+        print("updated position to    X: ", self.targetX,"    Y: ", self.targetY)
 
 if __name__ == '__main__':
     rospy.init_node('drive')
     d = driver()
     d.run()
+    d.listenForTarget()
