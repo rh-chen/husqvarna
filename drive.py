a86ea69a7f8c3cf8ca5e082833a7161316b17ecd
Jonas Karlsson <jonas@jonaskarlsson.xyz>
2018-10-17 10:10:25 +0200

    Added targetX and targetY functionality

--- drive.py
+++ drive.py
@@ -10,6 +10,7 @@ class driver():
         self.pub_twist = rospy.Publisher('/cmd_vel', Twist, queue_size=1)
         self.targetX = 0;
         self.targetY = 0;
+	self.target_subscriber = rospy.Subscriber('/target', Pose, self.updateTarget)
         print("init")
 
     def run(self):
@@ -22,22 +23,16 @@ class driver():
                 self.twist.linear.x = 1
                 self.twist.angular.z = 0.25
                 self.pub_twist.publish(self.twist)
-		        print("published")
                 r.sleep()
         except rospy.exceptions.ROSInterruptException:
-            pass
+            pass   
 
-    def listenForTarget(self):
-        self.target_subscriber = rospy.Subscriber('/target', Pose, self.updateTarget)
-
-
-    def updateTarget(msg):
-        self.targetX = msg.pose.position.x;
-        self.targetY = msg.pose.position.y;
+    def updateTarget(self, msg):
+        self.targetX = msg.position.x;
+        self.targetY = msg.position.y;
         print("updated position to    X: ", self.targetX,"    Y: ", self.targetY)
 
 if __name__ == '__main__':
     rospy.init_node('drive')
     d = driver()
     d.run()
-    d.listenForTarget()
